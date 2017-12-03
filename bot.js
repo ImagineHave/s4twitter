1ebc4b40371d9aff0fdff5a9f03a4227fbf88db9
cwih <chris.williams@imagine-have.xyz>
2017-12-03 17:13:43 +0000

    replier

--- bot.js
+++ bot.js
@@ -5,41 +5,36 @@ var
 
 var Twitter = new twit(config);
 
-// RETWEET BOT ==========================
+// ====
 
-// find latest tweet according the query 'q' in params
-var retweet = function() {
-    var params = {
-        q: '@realDonaldTrump',  // REQUIRED
-        result_type: 'recent',
-        lang: 'en'
-    }
-    Twitter.get('search/tweets', params, function(err, data) {
-      // if there no errors
-        if (!err) {
-          // grab ID of tweet to retweet
-            var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
-            });
-        }
-        // if unable to Search a tweet
-        else {
-          console.log('Something went wrong while SEARCHING...');
-        }
+// Call the stream function and pass in 'statuses/filter', our filter object, and our callback
+Twitter.stream('statuses/filter', {track: '@realDonaldTrump'}, function(stream) {
+
+  // ... when we get tweet data...
+  stream.on('data', function(tweet) {
+
+    // print out the text of the tweet that came in
+    console.log(tweet.text);
+
+    //build our reply object
+    var statusObj = {status: "Hi @" + tweet.user.screen_name + ", How are you?"}
+
+    //call the post function to tweet something
+    //Twitter.post('statuses/update', statusObj,  function(error, tweetReply, response){
+
+      //if we get an error print it out
+      if(error){
+        console.log(error);
+      }
+
+      //print the text of the tweet we sent out
+      console.log(tweetReply.text);
     });
-}
+  });
 
-// grab & retweet as soon as program is running...
-retweet();
-// retweet in every 50 minutes
-setInterval(retweet, 3000000);
+  // ... when we get an error...
+  stream.on('error', function(error) {
+    //print out the error
+    console.log(error);
+  });
+});
