a50e86f24d0db70d1cabbb3a047c479f96b30c78
cwih <chris.williams@imagine-have.xyz>
2017-12-03 17:17:30 +0000

    replier

--- bot.js
+++ bot.js
@@ -5,6 +5,46 @@ var
 
 var Twitter = new twit(config);
 
+// RETWEET BOT ==========================
+
+// find latest tweet according the query 'q' in params
+var retweet = function() {
+    var params = {
+        q: '@realDonaldTrump',  // REQUIRED
+        result_type: 'recent',
+        lang: 'en'
+    }
+    Twitter.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...');
+        }
+    });
+}
+
+// grab & retweet as soon as program is running...
+retweet();
+// retweet in every 50 minutes
+setInterval(retweet, 3000000);
+
+
 // ====
 
 // Call the stream function and pass in 'statuses/filter', our filter object, and our callback
@@ -23,18 +63,11 @@ Twitter.stream('statuses/filter', {track: '@realDonaldTrump'}, function(stream) 
     //Twitter.post('statuses/update', statusObj,  function(error, tweetReply, response){
 
       //if we get an error print it out
-      if(error){
-        console.log(error);
-      }
+      //if(error){
+        //console.log(error);
+      //}
 
       //print the text of the tweet we sent out
-      console.log(tweetReply.text);
+      //console.log(tweetReply.text);
     });
   });
-
-  // ... when we get an error...
-  stream.on('error', function(error) {
-    //print out the error
-    console.log(error);
-  });
-});
