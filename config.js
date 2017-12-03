81cb4f6cb5ceda273f88535c879b68f4ecc92718
cwih <chris.williams@imagine-have.xyz>
2017-12-03 16:54:17 +0000

    retweeter

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'xRgYRKp6Bm6HXsMGylmZnsjB8',  
+  consumer_secret: 'dXAwWTwTiQHvKDqN5tc5a6MrprgoIhFhUyV8DDvM1U30q6xEUb',
+  access_token: '937355461329850370-lbijYBXh0S8eBp6IwxM2mSq0vl9vWVD',  
+  access_token_secret: '0V8BNyPbN7uCXd9IIQeRMZG5HkfwYnjgENLGYgPVHNn93'
+}
