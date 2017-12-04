1b920b15d0fdafea5d7bc170637677b026b42558
cwih <chris.williams@imagine-have.xyz>
2017-12-04 19:12:39 +0000

    msf

--- bot.js
+++ bot.js
@@ -119,8 +119,8 @@ var reply = function() {
 
 var retweet = function() {
     var params = {
-       // q: '@MSF, @MindCharity, @amnesty, @SSChospices, @hrw, @UNHumanRights, @macmillancancer, @CR_UK, @NSPCC, #UKCharityWeek',  // REQUIRED
-        q: '#UKCharityWeek, #ukcharityweek',
+        q: '@MSF', //@MindCharity, @amnesty, @SSChospices, @hrw, @UNHumanRights, @macmillancancer, @CR_UK, @NSPCC, #UKCharityWeek',  // REQUIRED
+        //q: '#UKCharityWeek, #ukcharityweek',
         result_type: 'recent',
         lang: 'en'
     }
