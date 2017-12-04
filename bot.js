29c3b5e8f1a37657104ff22b72cad17e2c8e8583
cwih <chris.williams@imagine-have.xyz>
2017-12-04 13:50:54 +0000

    charities

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var screen_name = 'realDonaldTrump';
 
 // REPLY BOT ==========================
 
-var retweet = function() {
+var reply = function() {
 
     function cleanString(input) {
         var output = "";
@@ -116,8 +116,44 @@ var retweet = function() {
     });
 };
 
-// grab & retweet as soon as program is running...
-retweet();
-setInterval(retweet, 1000);
+
+var retweet = function() {
+    var params = {
+        q: '@Network4Africa, @MSF, @MindCharity, @amnesty, @SSChospices',  // REQUIRED
+        result_type: 'recent',
+        lang: 'en'
+    }
+    // for more parameters, see: https://dev.twitter.com/rest/reference/get/search/tweets
+
+    Twitter.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...');
+        }
+    });
+}
+
+
+reply();
+retweet()
+setInterval(reply, 1000);
+setInterval(reply, 900000);
 
 
