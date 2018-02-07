68ab10de02e0c06182a4d60a127d45bc46ee1376
cwih <chris.williams@imagine-have.xyz>
2018-02-07 18:58:51 +0000

    pass

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
-//config.js
+Z//config.js
 /** TWITTER APP CONFIGURATION
  * consumer_key
  * consumer_secret
  * access_token
  * access_token_secret
  */
+ 
+console.error(process.env.CONSUMER_KEY) 
 module.exports = {
   consumer_key: process.env.CONSUMER_KEY,  
   consumer_secret: process.env.CONSUMER_SECRET,
