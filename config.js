4be8b51b09f429d03723cf6ef2c5ea426d360f6a
cwih <chris.williams@imagine-have.xyz>
2018-02-07 18:50:43 +0000

    pass

--- config.js
+++ config.js
@@ -7,5 +7,7 @@
  */
 module.exports = {
   consumer_key: process.env.CONSUMER_KEY,  
-  consumer_secret: process.env.CONSUMER_SECRET
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,  
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
